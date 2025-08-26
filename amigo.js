59ab014f33ba057f856f4f959a336607134da1f0
AlexSandroSiqueiradeSouzaJunior <162588759+AlexSandroSiqueiradeSouzaJunior@users.noreply.github.com>
2025-08-26 08:59:06 -0300

    Add files via upload

--- amigo.js
+++ amigo.js
@@ -3,8 +3,14 @@ document.getElementById ('adicionar').onclick = function(){
     let amigo = document.getElementById('nome').value.trim();
     if(amigo){
         amigos.push(amigo)
+        document.getElementById ('nome'). value ="";
+        document.getElementById ('lista').innerHTML ='';
         for (let i= 0; i < amigos.length; i++){
-            document.getElementById('lista').innerHTML = `${amigos[i]}`
+            document.getElementById('lista').innerHTML += `<li>${amigos[i]}`
         }
     }
+}
+document.getElementById ('sortear').onclick = function(){
+    let sorteado = amigos[Math.floor(Math.random()*amigos.length)];
+    document.getElementById ('lista2').innerHTML =`<li>O sorteado é: ${sorteado}</li>`
 }
